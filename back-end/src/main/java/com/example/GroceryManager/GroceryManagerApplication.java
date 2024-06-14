6b1b70770a3c2b54941936776d6dfae1538412de
Maksim Aromäe <31857207+maarom@users.noreply.github.com>
2024-06-14 16:09:05 +0300

    Update GroceryManagerApplication.java

--- back-end/src/main/java/com/example/GroceryManager/GroceryManagerApplication.java
+++ back-end/src/main/java/com/example/GroceryManager/GroceryManagerApplication.java
@@ -2,19 +2,10 @@ package com.example.GroceryManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class GroceryManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryManagerApplication.class, args);
 	}
-
-
 }
